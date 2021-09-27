a0d1511fad604add73895bc69be65dccc0fe836a
soujinko <soujinko@naver.com>
2021-09-27 23:57:53 +0900

    ts react app setting

---
+++ src/components/test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Test = () => {
+
+
+    return (
+        <h1>hello 
+        <p style={{color: 'red'}}>test</p>
+        </h1>
+    )
+}
+
+export default Test;
